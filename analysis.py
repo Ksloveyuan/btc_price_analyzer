14ae498cb931e6d718de1d2412185a8b7fa7b731
Jeremy Liu <pliu@finxos.om>
2019-10-03 23:30:25 +0800

    refine chart

--- analysis.py
+++ analysis.py
@@ -7,26 +7,35 @@ from pyecharts.charts import Line, Candlestick
 import pyecharts.options as opts
 import datetime
 
+
 def close_ratio(close, base):
-    return close*1.0/base
+    return close * 1.0 / base
+
 
 if __name__ == '__main__':
-    price_data = pd.read_csv('price.csv', index_col=0, parse_dates=True, infer_datetime_format=True)
+    price_data = pd.read_csv('price.csv',
+                             index_col=0,
+                             parse_dates=True,
+                             infer_datetime_format=True)
 
-    price_data['Candle_Data'] = price_data.apply(lambda x: [x['openprice'], x['closeprice'], x['low'], x['high']], axis = 1)
+    price_data['Candle_Data'] = price_data.apply(
+        lambda x: [x['openprice'], x['closeprice'], x['low'], x['high']],
+        axis=1)
 
     ma_list = [100, 200]
 
     for ma in ma_list:
         ma_str = str(ma)
-        price_data['GMA_'+ ma_str] = price_data['closeprice'].rolling(window=ma, center=True, min_periods=1).apply(gmean, raw=True)  
+        price_data['GMA_' + ma_str] = price_data['closeprice'].rolling(
+            window=ma, center=True, min_periods=1).apply(gmean, raw=True)
 
-    
     for ma in ma_list:
         ma_str = str(ma)
-        price_data["GMA_Ratio_"+ma_str] = price_data.apply(lambda x: close_ratio(x['closeprice'],x['GMA_'+ma_str]), axis = 1)
+        price_data["GMA_Ratio_" + ma_str] = price_data.apply(
+            lambda x: close_ratio(x['closeprice'], x['GMA_' + ma_str]), axis=1)
 
-    price_data["Predict_Ratio"] = price_data.apply(lambda x: close_ratio(x['closeprice'],x['predictprice']), axis = 1)
+    price_data["Predict_Ratio"] = price_data.apply(
+        lambda x: close_ratio(x['closeprice'], x['predictprice']), axis=1)
 
     price_data = price_data.sort_index(axis=0, ascending=True)
 
@@ -34,43 +43,73 @@ if __name__ == '__main__':
 
     candle = Candlestick()
     candle.add_xaxis(time).set_series_opts(type='time')
-    candle.add_yaxis("data", price_data['Candle_Data'].tolist())
+    candle.add_yaxis("日K", price_data['Candle_Data'].tolist())
 
     line1 = Line()
     line1.add_xaxis(time).set_series_opts(type='time')
     for ma in ma_list:
         ma_str = str(ma)
-        line1.add_yaxis("GMA_"+ma_str,price_data['GMA_'+ma_str].tolist(),yaxis_index=0, is_symbol_show=False)
+        line1.add_yaxis("GMA_" + ma_str,
+                        price_data['GMA_' + ma_str].tolist(),
+                        yaxis_index=0,
+                        is_symbol_show=False)
 
     line2 = Line()
     line2.add_xaxis(time).set_series_opts(type='time')
     for ma in ma_list:
         ma_str = str(ma)
-        line2.add_yaxis("GMA_Ratio_"+ma_str,price_data['GMA_Ratio_'+ma_str].tolist(),yaxis_index=1, is_symbol_show=False)
+        line2.add_yaxis("GMA_Ratio_" + ma_str,
+                        price_data['GMA_Ratio_' + ma_str].tolist(),
+                        yaxis_index=1,
+                        is_symbol_show=False)
 
-    line2.add_yaxis("Predict_Ratio",price_data['Predict_Ratio'].tolist(),yaxis_index=1, is_symbol_show=False)
-    line2.set_series_opts(markline_opts=opts.MarkLineOpts(data=[opts.MarkLineItem(symbol='none', y=1)], linestyle_opts=opts.LineStyleOpts(width=1, type_='dashed')))
+    line2.add_yaxis("Predict_Ratio",
+                    price_data['Predict_Ratio'].tolist(),
+                    yaxis_index=1,
+                    is_symbol_show=False)
+    line2.set_series_opts(markline_opts=opts.MarkLineOpts(
+        precision=2,
+        is_silent=True,
+        data=[
+            opts.MarkLineItem(y=1.2),
+            opts.MarkLineItem(y=1),
+            opts.MarkLineItem(y=0.8),
+        ],
+        linestyle_opts=opts.LineStyleOpts(width=1, type_='dashed'),
+        label_opts=opts.LabelOpts(is_show=True, position='middle')))
 
     line3 = Line()
     line3.add_xaxis(time).set_series_opts(type='time')
-    line3.add_yaxis('Predict', price_data['predictprice'].tolist(), yaxis_index=0, is_symbol_show=False)
+    line3.add_yaxis('Predict',
+                    price_data['predictprice'].tolist(),
+                    yaxis_index=0,
+                    is_symbol_show=False)
 
     line1.extend_axis(yaxis=opts.AxisOpts())
 
     line1.set_global_opts(
-        title_opts=opts.TitleOpts(title="Line-基本示例"),
+        title_opts=opts.TitleOpts(title="Bitcoin"),
         xaxis_opts=opts.AxisOpts(type_="category", name="x"),
         yaxis_opts=opts.AxisOpts(
             # type_="log",
-            splitline_opts=opts.SplitLineOpts(is_show=True), 
-            is_scale=True,),
+            splitline_opts=opts.SplitLineOpts(is_show=False),
+            is_scale=True,
+        ),
         tooltip_opts=opts.TooltipOpts(trigger="axis", trigger_on="mousemove"),
         datazoom_opts=[
             opts.DataZoomOpts(xaxis_index=0, range_start=80, range_end=100)
-        ],)
-    
+        ],
+        visualmap_opts=opts.VisualMapOpts(min_=0.2,
+                                          max_=2,
+                                          pos_top=30,
+                                          pos_right=10,
+                                          split_number=100,
+                                          series_index=[3, 4, 5],
+                                          out_of_range={'color': '#999999'}),
+    )
+
     line1.overlap(line3)
     line1.overlap(line2)
     line1.overlap(candle)
-         
+
     line1.render("chart.html")
--- crawler.py
+++ crawler.py
@@ -5,19 +5,22 @@ import numpy as np
 import datetime
 import math
 
-
 if __name__ == '__main__':
-    base_day = datetime.datetime(2009,1,3)
-    price_data = pd.DataFrame(columns=['tickertime', 'openprice', 'closeprice', 'high', 'low', 'predictprice'])
+    base_day = datetime.datetime(2009, 1, 3)
+    price_data = pd.DataFrame(columns=[
+        'tickertime', 'openprice', 'closeprice', 'high', 'low', 'predictprice'
+    ])
 
     header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+        "User-Agent":
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
     }
 
     pages = range(1, 8)
     count = 0
     for page in pages:
-        url = 'http://dncapi.bqiapp.com/api/v3/coin/history?coincode=bitcoin&begintime=20090829&endtime=20191230&page={}&per_page=1000&webp=1'.format(page)
+        url = 'http://dncapi.bqiapp.com/api/v3/coin/history?coincode=bitcoin&begintime=20090829&endtime=20291230&page={}&per_page=1000&webp=1'.format(
+            page)
         web_data = requests.get(url, headers=header)
         web_data.encoding = 'utf-8'
         json_obj = json.loads(web_data.text)
@@ -27,9 +30,16 @@ if __name__ == '__main__':
             date = item.get("tickertime")[0:10]
             diff = datetime.datetime.strptime(date, '%Y-%m-%d') - base_day
             # predictPrice = (diff.days/693)**5.526
-            predictPrice2 = 10 ** (-17.01593313+5.84509376*math.log10(diff.days))
-            row = np.array([date, item.get("openprice"),item.get("closeprice"),item.get("high"),item.get("low"), predictPrice2])
+            predictPrice2 = 10**(-17.01593313 +
+                                 5.84509376 * math.log10(diff.days))
+            row = np.array([
+                date,
+                item.get("openprice"),
+                item.get("closeprice"),
+                item.get("high"),
+                item.get("low"), predictPrice2
+            ])
             price_data.loc[count] = row
             count = count + 1
-    
-    price_data.to_csv("price.csv",index=False,sep=',')
+
+    price_data.to_csv("price.csv", index=False, sep=',')
